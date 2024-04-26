924f72313fa51f3e7fbfedc2cbddefbfa3feb945
Sebastian Cozzi <sebacozzi26@gmail.com>
2024-04-26 00:49:47 -0300

    Agregando practicas JS

--- practica/js/form.js
+++ practica/js/form.js
@@ -34,7 +34,7 @@ function tipoDeValor() {
 }
 
 function mayorMenor() {
-    
+
     const vA = parseFloat(valorPorID('datoA'));
     const vB = parseFloat(valorPorID('datoB'));
     const resu = elemento('resultado2');
@@ -53,3 +53,61 @@ function mayorMenor() {
         return;
     }
 }
+
+asignarClick('calcularSiMayor', verifiMayor);
+
+function verifiMayor() {
+    const edad = parseInt(valorPorID('edad'));
+    const esMayor = (edad) => { return edad >= 18 };
+    const resu = elemento('resultado3');
+    if (isNaN(edad)) {
+        alert('No se ha ingresado una edad!!!!!!!!!!');
+        return;
+    }
+    if (esMayor(edad)) {
+        resu.innerText = 'Es mayor de edad';
+    } else {
+        resu.innerText = 'Es menor de edad';
+    }
+}
+
+asignarClick('agregarNum', agregaALista)
+
+const lista = [];
+
+function agregaALista() {
+    const valor = parseInt(valorPorID('numero'));
+    if (isNaN(valor)) {
+        alert('"' + valorPorID('numero') + '" no es número!');
+        return;
+    }
+
+    lista.push(valor);
+
+    elemento('lista1').innerText = lista.toString();
+    elemento('numero').value = "";
+}
+
+asignarClick('buscaMayor', buscaMayor);
+
+
+
+function buscaMayor() {
+    let mayor = undefined;
+
+    const resu = elemento('resultado4');
+
+    if (lista.length) {
+        if (lista.length == 1) {
+            resu.innerText = 'La lista tiene 1 solo dato y es ' + lista[0];
+        } else {
+            mayor = lista[0];
+            lista.forEach((v) => { if (v > mayor) { mayor = v } });
+            resu.innerText = 'El mayor de la lista es ' + mayor;
+        }
+    } else {
+        alert('La lista está vacia');
+    }
+}
+
+asignarClick('buscaMenor', buscaMenor);
