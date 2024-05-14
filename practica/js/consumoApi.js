2785216171ed0625c8262e8e85dde5c7556a49d8
sebacozzi <127224339+sebacozzi@users.noreply.github.com>
2024-05-14 15:05:27 -0300

    Practicas con Fetch y consumo de api

--- practica/js/consumoApi.js
+++ practica/js/consumoApi.js
@@ -1,34 +1,63 @@
-
-async function cargaAPI(url) {
-    let resultado;
-    console.log('url antes del fetch: ', url);
-    const respuesta = await fetch(url);
-    return respuesta.json();
-}
+let indicePagina = 1;
 let datos;
 
+async function cargaAPI(url) {
+    const respuesta = await fetch(url);
+    datos = await respuesta.json();
+}
+
 async function obtenerDatos() {
-    const url = document.getElementById('url').value;
-    datos = await cargaAPI(url);
+    const url ='https://rickandmortyapi.com/api/character?page=1';
+    await cargaAPI(url);
+     
+     mostrarDatos();
+}
+
+
+function mostrarDatos() {
     document.getElementById('respuesta').innerHTML = '';
-
-    document.getElementById('siguiente').attributes.disabled = datos.page < datos.total_page;
-    //document.getElementById('anterior').attributes.disabled = datos.page === 1;
-    console.log(document.getElementById('siguiente').attributes)
-
-
-    datos.data.forEach(pers => {
-        document.getElementById('respuesta').innerHTML += carta(pers.first_name, pers.last_name, pers.avatar, pers.email);
+    chequearBotones();
+    datos.results.forEach(pers => {
+        document.getElementById('respuesta').innerHTML += carta(pers.name, pers.id, pers.image, pers.status, pers.species);
     });
 }
 
-function carta(nombre, apellido, urlImg, email) {
+
+function chequearBotones() {
+    document.getElementById('siguiente').disabled = datos.info.next == null;
+        document.getElementById('anterior').disabled = datos.info.prev == null;
+}
+
+function carta(nombre, id, urlImg, estado,especie) {
     return `<div class="card" style="width: 18rem; margin:10px;">
         <img class="card-img-top" src="${urlImg}" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">${nombre} ${apellido}</h5>
-                <p class="card-text">${email}</p>
+                <h5 class="card-title">${nombre} (${id})</h5>
+                <p class="card-text">Estado: ${estado=='Alive'?'Vivo':estado=='Dead'? 'Muerto':'Desconocido'}.</p>
+                <p class="card-text">Especie: ${especie=='Human'?'Humano':estado=='Alien'? 'Alien':'Desconocido'}.</p>
+
                 <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
             </div>
     </div>`
 }
+
+async function botonSiguiente(ev) {
+        if (datos.info.next){
+            indicePagina ++;
+            const url = datos.info.next;
+            await cargaAPI(url);
+        }
+            mostrarDatos();
+}
+
+async function botonAnterior(ev){
+    if (datos.info.prev){
+        indicePagina --;
+        const url = datos.info.prev;
+        await cargaAPI(url);
+    }
+        mostrarDatos();
+}
+
+document.getElementById('anterior').addEventListener('click',botonAnterior);
+document.getElementById('siguiente').addEventListener('click',botonSiguiente);
