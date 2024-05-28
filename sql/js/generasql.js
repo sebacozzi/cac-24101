bdc9cc3db3f4aab2c57764371b84ea7fbb82b7c3
sebacozzi <127224339+sebacozzi@users.noreply.github.com>
2024-05-28 15:05:06 -0300

    Proceso de generación de peliculas

--- sql/js/generasql.js
+++ sql/js/generasql.js
@@ -1,62 +1,142 @@
 //Script generar datos
 
 const botonGenerar = document.querySelector('#generar');
+const bDescarga = document.querySelector('#descargar');
+const paises = ['Argentina', 'Brasil', 'Uruguay', 'Estados Unidos', 'España', 'Mexico', 'Colombia', 'Bolivia', 'Chile', 'Francia']
+
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTJhNWYwMWYyNDE5NjU4MzIyNjgyNTFkMmVkMzUxNiIsInN1YiI6IjY2NGQ2NDEyNzdlZDQxMTZiZjEzZWMxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wHAgiz8Ui-_jhnXCnY11VgdlT6M3IUARpmX3Pb0_KTM'
+    }
+};
+
+bDescarga.addEventListener('click', descargarSQL);
 
 botonGenerar.addEventListener('click', generar);
 
+function descargarSQL() {
+    let sql = document.querySelectorAll('code');
+    let archivo = '';
+    for (let i = 0; i < sql.length; i++) {
+        if (sql[i].innerHTML != '') {
+            archivo = archivo + sql[i].innerHTML + '\n';
+        } else {
+            archivo = archivo + '/* ' + sql[i].attributes['descripcion'].value + ' no generado' + ' */\n';
+        }
 
+    }
+    console.log(archivo);
+    bDescarga.href = "data:application/octet-stream,"
+        + encodeURIComponent(archivo);
+    bDescarga.download = 'archivo.sql';
+
+}
+
+function randomPais() {
+    return paises[parseInt(Math.random() * paises.length - 1)]
+}
+
+function convertirFecha(fecha) {
+
+    return fecha.split('T')[0];
+}
+
+function usuarioAString(usuario) {
+    return `("${usuario.name.first}","${usuario.name.last}","${usuario.email}","${convertirFecha(usuario.dob.date)}","${usuario.location.country}")`;
+}
 async function generar(ev) {
 
     const contDirectores = parseInt(document.querySelector('#count-directores').value);
     const contPelis = parseInt(document.querySelector('#count-peliculas').value);
     const contUsuarios = parseInt(document.querySelector('#count-usuarios').value);
+    const insertUsuarios = document.querySelector('#insert_usuarios');
+    const insertDirectores = document.querySelector('#insert_usuarios');
+    const insertPeliculas = document.querySelector('#insert_usuarios');
 
-    console.log('directores: ', contDirectores, '; peliculas: ', contPelis, ' usuarios: ', contUsuarios)
-
-    //const listaDirectores = await generarDatosDirector();
     let listaUsuarios = [];
-    let paso = '';
-    for (let i = 0; i < contUsuarios; i++) {
-        paso = paso + '.';
-        listaUsuarios.push(await generarDatosUsuarios(paso));
+
+    insertUsuarios.innerHTML = 'Generando Usuarios...';
+
+   /*  const usuarios = await generarDatosUsuarios(contUsuarios);
+
+    for (const usuario of usuarios.results) {
+        listaUsuarios.push(usuarioAString(usuario));
     }
 
-    document.querySelector('#insert_usuarios').innerHTML = `INSERT INTO usuarios(nombre,apellido,email,fecha_nac,pais) VAULES (\n\t${listaUsuarios.join(', \n\t')});`;
+    insertUsuarios.innerHTML = `INSERT INTO usuarios(nombre,apellido,email,fecha_nac,pais) VAULES (\n\t${listaUsuarios.join(', \n\t')});`;
+ */
+    let listaPeliculas = await generarListaPeliculas(contPelis);
 
-    let listaDirectores = [];
-    paso = '';
-    for (let i = 0; i < contDirectores; i++) {
-        paso = paso + '.';
-        listaDirectores.push(await generarDatosDirector(paso));
-    }
 
+    console.log(listaPeliculas)
+    let peli = await detallePelicula(listaPeliculas.results[0].id);
+    console.log('PELI::: ',peli);
+    const director = await detalleDirector(peli.crew[0].id);
+    console.log(await detalleDirector(peli.crew[0].id))//listaDirectores.results[0].id));
 
 }
 
 // apis
 
-async function generarDatosDirector(step) {
-    await fetch('https://randomuser.me/api/')
+async function generarListaPeliculas(count) {
+    let lista=[];
+    const url = `https://api.themoviedb.org/3/movie/popular?language=es`;
+    const repetir = Math.ceil(count / 20);
+    while (repetir ===0) {
+        repetir --;
+        
+        let result;
+        await fetch(url, options)
         .then(response => response.json())
-        .then(json => {
-            document.querySelector('#insert_directores').innerHTML = 'generando directores' + step;
-            console.log(json);
-        })
+        .then(response => result = response)
+        .catch(err => console.error(err));
+for (const peli of result.results) {
+
+    }                
+           
+        }
+        count -=20;
+    }
+
+     
+        
+    return lista;
+}
+
+async function detalleDirector(id) {
+    let result;
+    const url = `https://api.themoviedb.org/3/person/${id}`;
+
+
+    await fetch(url, options)
+        .then(response => response.json())
+        .then(response => result = response)
+        .catch(err => console.error(err));
+    return result;
+    /* await fetch(url)
+        .then(response => response.json())
+        .then(json => result = json) */
 
 }
-async function generarDatosUsuarios(step) {
+async function generarDatosUsuarios(count) {
     let result;
-    await fetch('https://randomuser.me/api/')
+    const url = `https://randomuser.me/api/?results=${count}&nat=es,br,mx,ch,ca&inc=name,location,email,dob`;
+    await fetch(url)
         .then(response => response.json())
-        .then(json => {
-            document.querySelector('#insert_usuarios').innerHTML = 'generando Usuarios' + step;
-            if ('IranNorwayTurkeySerbia'.includes(json.results[0].location.country)) {
-                result = generarDatosUsuarios(step);
-            } else {
-                result =
-                    `(${json.results[0].name.first},${json.results[0].name.last}, ${json.results[0].email}, ${json.results[0].dob.date}, ${json.results[0].location.country})`;
-            }
-        })
+        .then(json => result = json)
 
     return result;
 }
+
+async function detallePelicula(id) {
+    let result;
+    console.log('detalle Pelicula: ', id)
+
+    await fetch(`https://api.themoviedb.org/3/movie/${id}/credits?language=es`, options)
+        .then(response => response.json())
+        .then(response => result = response);
+    return result;
+}
+
